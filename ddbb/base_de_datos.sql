6eb87eeaf7f9907d79d26115478e9581539e3a70
Lucaster85 <lucasdc87@gmail.com>
2020-10-15 19:00:20 -0300

    first commit

---
+++ ddbb/base_de_datos.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS curso_backfront;
+USE curso_backfront;
+
+CREATE TABLE users(
+    id          int(255) not null auto_increment,
+    role        VARCHAR(20),
+    name        VARCHAR(100),
+    surname     VARCHAR(100),
+    email       VARCHAR(255),
+    password    VARCHAR(255),
+    created_at   datetime,
+    CONSTRAINT  pk_users PRIMARY KEY(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE tasks(
+    id          int(255) not null auto_increment,
+    user_id     int(255) not null,
+    title       VARCHAR(255),
+    description text,
+    status      VARCHAR(100),
+    created_at  datatime DEFAULT CURRENT_TIMESTAMP,
+    updated_at  datatime,
+    CONSTRAINT pk_tasks PRIMARY KEY(id),
+    CONSTRAINT fk_tasks_users FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
